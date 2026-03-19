60c1c6dc85306193d7a4b85782a7d66466a25f79
Agnish Bhattacharya <sukla.baruipur2@gmail.com>
2026-03-19 19:03:02 +0530

    feat: change open ai api to gemini

--- lib/workflow/executor/extractDataWithAI.ts
+++ lib/workflow/executor/extractDataWithAI.ts
@@ -2,7 +2,7 @@ import { ExecutionEnvironment } from '@/utils/types/executor';
 import { ExtractDataWithAITask } from '../task/extract-data-with-ai';
 import prisma from '@/lib/prisma';
 import { symmetricDecrypt } from '@/lib/encryption';
-import OpenAI from 'openai';
+import { GoogleGenerativeAI } from '@google/generative-ai';
 
 export async function ExtractDataWithAIExecutor(environment: ExecutionEnvironment<typeof ExtractDataWithAITask>): Promise<boolean> {
     try {
@@ -41,33 +41,18 @@ export async function ExtractDataWithAIExecutor(environment: ExecutionEnvironmen
             return false;
         }
 
-        const openai = new OpenAI({
-            apiKey: plainCredentialValue,
+        const genAI = new GoogleGenerativeAI(plainCredentialValue);
+        const model = genAI.getGenerativeModel({ 
+            model: "gemini-1.5-flash",
+            systemInstruction: 'You are a webscraper helper that extracts data from HTML or text. You will be given a piece of text or HTML content as input and also the prompt with the data you have to extract. The response should always be only the extracted data as a JSON array or object, without any additional words or explanations. Analyze the input carefully and extract data precisely based on the prompt. If no data is found, return an empty JSON array. Work only with the provided content and ensure the output is always a valid JSON array without any surrounding text'
         });
         
-        const response = await openai.chat.completions.create({
-            model: 'gpt-3.5-turbo',
-            messages: [
-                {
-                    role: 'system',
-                    content: 'You are a webscraper helper that extracts data from HTML or text. You will be given a piece of text or HTML content as input and also the prompt with the data you have to extract. The response should always be only the extracted data as a JSON array or object, without any additional words or explanations. Analyze the input carefully and extract data precisely based on the prompt. If no data is found, return an empty JSON array. Work only with the provided content and ensure the output is always a valid JSON array without any surrounding text'
-                },
-                {
-                    role: 'user',
-                    content: content
-                },
-                {
-                    role: 'user',
-                    content: prompt
-                },
-            ],
-            temperature: 0.7,
-        });
+        const response = await model.generateContent(`Content:\n${content}\n\nPrompt:\n${prompt}`);
         
-        environment.log.info(`Prompt tokens: ${response.usage?.prompt_tokens}`);
-        environment.log.info(`Completition tokens: ${response.usage?.completion_tokens}`);
+        environment.log.info(`Prompt tokens: ${response.response.usageMetadata?.promptTokenCount}`);
+        environment.log.info(`Completition tokens: ${response.response.usageMetadata?.candidatesTokenCount}`);
 
-        const result = response.choices[0].message?.content;
+        const result = response.response.text();
         if (!result) {
             environment.log.error('Empty response from AI');
             return false;
